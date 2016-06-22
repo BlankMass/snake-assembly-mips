f5e4a978e127116ba22c8193e268ef64b049a89b
Mikael Mello <mikaelmello@hotmail.com>
2016-06-22 18:11:02 -0300

    Fixed movement system, added to-dos

--- main.s
+++ main.s
@@ -1,5 +1,5 @@
 .data
-SSL:	.word 0, 6, 3			# Score, Size and Lives
+SSL:	.word 0, 5, 3			# Score, Size and Lives
 HPTP:	.word  37, 24, 37, 25	# Head position (X, Y) and Tail position (X, Y) 0 <= X <= 75, 0 <= Y <= 49
 CD: 	.word 1				# Current direction. WASD = 1234
 GRID: 	.space 3800			# Number of bytes of a 304x200 pixels grid for the game (76x50 units)
@@ -7,8 +7,7 @@ GRID: 	.space 3800			# Number of bytes of a 304x200 pixels grid for the game (76
 	jal FILLSCREEN
 	jal FILLGAME
 	jal INITSNAKE
-	
-		
+			
 	GAMELOOP:
 		la $t0, HPTP
 		lw $s0, 0($t0)
@@ -19,35 +18,16 @@ GRID: 	.space 3800			# Number of bytes of a 304x200 pixels grid for the game (76
 		la $t0, CD
 		lw $s4, 0($t0)		# Current Direction
 		
-		la $t1,0xFF000000
-		lw $t0,0($t1)
-		andi $t0,$t0,0x0001		# Le bit de Controle Teclado
-   		beq $t0,$zero,PULA   	   	# Se não há tecla pressionada PULA (nao realiza nenhuma acao)
-  		lw $v0,4($t1)  			# Tecla lida
+		li $v0, 12
+		syscall
+   		beq $v0,$zero,PULA   	   	# Se não há tecla pressionada PULA (nao realiza nenhuma acao)
   		
 		addi $t1, $s6, 8
 		mulu $t2, $s0, 4
 		addu $t1, $t1, $t2
 		mulu $t2, $s1, 1280
 		addu $t1, $t1, $t2	# $t1 = address of head
-		
-		li $t2, 0
-		sw $t2, 0($t1)
-		sw $t2, 320($t1)
-		sw $t2, 640($t1)
-		sw $t2, 960($t1)	# Transforms the head into a part of the body (changes the color)
-		
-		addi $t3, $s6, 8
-		mulu $t2, $s2, 4
-		addu $t3, $t3, $t2
-		mulu $t2, $s3, 1280
-		addu $t3, $t3, $t2	# $t1 = address of last piece of body
-		
-		li $t2, 0xEFEFEFEF
-		sw $t2, 0($t3)
-		sw $t2, 320($t3)
-		sw $t2, 640($t3)
-		sw $t2, 960($t3)	# Deletes the last piece of the body
+		addu $t3, $t1, $zero
 	
 	# Testa as teclas e atualiza dados de acordo com elas.
 	W:	bne $v0, 119, A
@@ -72,14 +52,20 @@ GRID: 	.space 3800			# Number of bytes of a 304x200 pixels grid for the game (76
 		beq $s4, 2, NOT
 		addi $s0, $s0, 1	# Atualiza (X,Y) e endereco da cabeca
 		addi $t1, $t1, 4
-		
 		addi $s4, $zero, 4	# Atualiza direcao atual
-		
+				
 		j NOTNOT
 		
 	PULA: 	j NOT
 	
 	NOTNOT:
+		
+		li $t2, 0
+		sw $t2, 0($t3)
+		sw $t2, 320($t3)
+		sw $t2, 640($t3)
+		sw $t2, 960($t3)	# Transforms the head into a part of the body (changes the color)
+		
 		li $t0, 0x22222222
 		sw $t0, 0($t1)
 		sw $t0, 320($t1)
@@ -87,16 +73,68 @@ GRID: 	.space 3800			# Number of bytes of a 304x200 pixels grid for the game (76
 		sw $t0, 960($t1)	# filling head
 		la $t0, HPTP
 		sw $s0, 0($t0)
-		sw $s1, 4($t0)
+		sw $s1, 4($t0)		# Updating head coordinates
 		la $t0, CD
-		sw $s4, 0($t0)
-		li $v0, 0
-	
+		sw $s4, 0($t0)		# Updating current direction
+		
+		
+		addi $t3, $s6, 8
+		mulu $t2, $s2, 4
+		addu $t3, $t3, $t2
+		mulu $t2, $s3, 1280
+		addu $t3, $t3, $t2	# $t1 = address of last piece of body
+		
+		li $t2, 0xEFEFEFEF
+		sw $t2, 0($t3)
+		sw $t2, 320($t3)
+		sw $t2, 640($t3)
+		sw $t2, 960($t3)	# Deletes the last piece of the body
+		
+		add $a0, $s4, $zero
+		jal UPDTSTACK
+		
+		
+		
 	NOT:
 		
 		j GAMELOOP
 		j END
+		
+UPDTSTACK:	
+	lw $t0, 0($sp)
+	la $t1, HPTP
+	lw $t2, 8($t1)
+	lw $t3, 12($t1)
+	bne $t0, 1, A2
+	addi $t3, $t3, -1
+	A2:
+	bne $t0, 2, S2
+	addi $t2, $t2, -1
+	S2:
+	bne $t0, 3, D2
+	addi $t3, $t3, +1
+	D2:
+	bne $t0, 4, NOTD2
+	addi $t2, $t2, +1
+	NOTD2:
+	sw $t2, 8($t1)
+	sw $t3,12($t1)		# Updating new tail's position
 	
+	la $t2, SSL
+	lw $t3, 4($t2)
+	addi $t3, $t3, -1
+	li $t1, 0
+	add $t4, $sp, $zero
+	LOOP11:
+		beq $t1, $t3, OUTLOOP11
+		lw $t5, 4($t4)
+		sw $t5, 0($t4)
+		addi $t4, $t4, 4	
+		addi $t1, $t1, 1
+		j LOOP11
+	OUTLOOP11:
+		sw $a0, 0($t4)
+		jr $ra
 # Fills the screen with the border color
 FILLSCREEN:
 	li $t1,0xFF012C00
@@ -147,11 +185,15 @@ LOOP3:	beq $t3, $t4, OUTLOOP
 	sw $s4, 320($t1)
 	sw $s4, 640($t1)
 	sw $s4, 960($t1)	# filling tail
+	addi $sp, $sp, -4
+	li $t7, 1
+	sw $t7, 0($sp)
 	addiu $t1, $t1, 960
 	addi $t3, $t3, 1
 	j LOOP3
 OUTLOOP:
-	li $t3, 29
+
+	add $t3, $s1, $t4
 	sw $t3, 12($t0)
 	jr $ra
 	
@@ -178,6 +220,3 @@ T:	addi $s2,$s2,4
 FORA2:	jr $ra
 
 END:
-
-.ktext
-	eret
